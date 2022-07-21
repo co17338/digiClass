d8e93d6cc39968e0f1ee14541bf75efe70c6ee04
Monarch Hasija <monarchasija99@gmail.com>
2022-07-21 08:37:34 +0530

    create class

--- src/Pages/Teacher/CreateClass.js
+++ src/Pages/Teacher/CreateClass.js
@@ -2,11 +2,23 @@ import {Component} from "react";
 import { connect } from 'react-redux';
 
 class CreateClass extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            "className":"",
+            "hostId":"",
+            "timeFrom":"",
+            "timeTo":"",
+            "days":[0,0,0,0,0,0,0],
+            "desc":"",
+        }
+    }
+
     render(){
         return(
             <>
                 CreateClass
-                {this.props.name}
             </>
         )
     }
