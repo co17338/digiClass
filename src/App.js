6c103c7ad99959326e56183b274f8e317e711168
Monarch Hasija <monarchasija99@gmail.com>
2022-07-21 20:19:39 +0530

    page correction

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Component} from 'react';
 import  { BrowserRouter as Router ,Route, Routes} from 'react-router-dom';
 import './App.css';
 import T_Home from './Pages/Teacher/Home';
+import S_Home from './Pages/Student/Home';
 
 class App extends Component{
 
@@ -9,7 +10,8 @@ class App extends Component{
         return(
             <Router>
                 <Routes>
-                    <Route path='/' exact element={<T_Home/>} />
+                    <Route path='/t/' exact element={<T_Home/>} />
+                    <Route path='/s/' exact element={<S_Home/>} />
                 </Routes>
             </Router>
         )
