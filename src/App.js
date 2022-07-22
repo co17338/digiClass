6a02d08574873d9bd0cd5a6e02422faa85ff3d92
Monarch Hasija <monarchasija99@gmail.com>
2022-07-22 19:15:39 +0530

    login/signup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import  { BrowserRouter as Router ,Route, Routes} from 'react-router-dom';
 import './App.css';
 import T_Home from './Pages/Teacher/Home';
 import T_Profile from './Pages/Student/Profile'
+import T_Login from './Pages/Teacher/Login'
 import S_Home from './Pages/Student/Home';
 
 class App extends Component{
@@ -12,6 +13,7 @@ class App extends Component{
             <Router>
                 <Routes>
                     <Route path='/t/' exact element={<T_Home/>} />
+                    <Route path='/t/Login' exact element={<T_Login/>} />
                     <Route path='/t/profile' exact element={<T_Profile/>} />
                     <Route path='/s/' exact element={<S_Home/>} />
                 </Routes>
--- src/Pages/Teacher/Login.js
+++ src/Pages/Teacher/Login.js
@@ -2,11 +2,37 @@ import {Component} from "react";
 import { connect } from 'react-redux';
 //sahil
 class T_Login extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isLogin:true,
+            "username":"",
+            "password":""
+        }
+    }
+
+    handleusername = (event) =>{
+        this.setState({"username":event.target.value})
+    }
+
+    handlepassword = (event) =>{
+        this.setState({"password":event.target.value})
+    }
+
     render(){
         return(
             <>
-                Teacher Login
-                {this.props.name}
+                { this.state.isLogin && <p>Login</p>}  
+                { !this.state.isLogin && <p>Signup</p>}
+
+                <button  onClick={()=>this.setState({isLogin: !(this.state.isLogin)  })}>Toggle</button>
+               <br/><br/><br/><br/>
+                                {/* {this.setState({isLogin:false})} */}
+                Teacher Login<br/>
+                username<input type="text" onChange={this.handleusername} label="username" /><br/>
+                password<input type="password" onChange={this.handlepassword} label="password" />
+
             </>
         )
     }
